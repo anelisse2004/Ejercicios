617971ee517585c583acc46158f7c01f2921b65f
anelisse2004 <113318741+anelisse2004@users.noreply.github.com>
2022-09-11 20:23:34 -0600

    Add files via upload

---
+++ Período 3/Unidad#3Semana#2Clase#5Ejercicio#1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Escriba una nota:";
+	char nota;
+	cin>> nota;
+	
+	switch (nota){
+		case 'A': case 'a':
+		cout<< "Excelente, aprobado con honores";
+		break;
+		case 'B': case'b':
+		cout<< "Aprobado de forma sobresaliente";
+		break;
+		case 'C': case 'c':
+		cout<< "Aprobado con buen desempeño" << endl<<"o";
+		break;
+		case 'D': case 'd':
+		cout<<"Aprobado de manera regular";
+		break;
+		case 'E': case 'e':
+		cout<< "Aprobado, pero se sugiere mejorar";
+		break;
+		case 'F': case 'f':
+		cout << "Reprobado, con rendimiento pobre";
+		break;
+		default:
+		cout<<"**ERROR, nota ingresada es incorrecta**";
+		
+	}// fin switch nota
+	
+}
+
